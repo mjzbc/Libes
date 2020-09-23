9fcbe7101eb6ad28161b722a316c07356640d629
theSuco <miguelipince@gmail.com>
2020-09-23 16:49:52 -0400

    Books routes and Author update

---
+++ models/book.js
@@ -0,0 +1,48 @@
+const mongoose = require('mongoose');
+const path = require('path');
+
+// create var for multer multipart form-data
+const coverImageBasePath = 'uploads/bookCovers';
+
+const bookSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+    },
+    publishDate: {
+        type: Date,
+        required: true,
+    },
+    pageCount: {
+        type: Number,
+        required: true,
+    },
+    createdAt: {
+        type: Date,
+        required: true,
+        default: Date.now
+    },
+    coverImageName: {
+        type: String,
+        required: true,
+    },
+    author: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true,
+        ref: 'Author'
+    }
+});
+
+// create virtual property, derives value on model above
+bookSchema.virtual('coverImagerPath').get(function() {
+    //use 'function' keyword, not arrow function, need access to 'this' keyword for the book
+    if (this.coverImageName != null) {
+        return path.join('/', coverImageBasePath, this.coverImageName)
+    }
+})
+
+module.exports = mongoose.model('Book', bookSchema);
+module.exports.coverImageBasePath = coverImageBasePath;
